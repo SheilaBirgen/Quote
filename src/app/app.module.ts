53a341fdebb106e2ca261758ae455ebef81f3a2d
SheilaBirgen <jeronobergen@gmail.com>
2020-01-13 09:21:29 +0300

    changed form details in quote-form.components.html -added quote details to quote-details.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighlightPipe } from './highlight.pipe';
+import { DateCountPipe } from './date-count.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    HighlightPipe,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,15 +1,21 @@
-import { stringify } from 'querystring';
-
 export class Quote {
-    showDescription:boolean;
-    constructor(
-       public id: number,
-       public name: string,
-       public description:string,
-       public Date:Date
-    ){
-        this.showDescription=false;
-    }
+    
+    showDescription: boolean;
+
+        constructor(
+            public id: number, 
+            public quote: string, 
+            public author: string, 
+            public by:string, 
+            public complete: Date,
+            public likeNo: number,
+            public dislikeNo: number
+            )
+        {
+            this.showDescription = false;
+            this.likeNo=0;
+            this.dislikeNo=0;
+        }
 
 }
 
